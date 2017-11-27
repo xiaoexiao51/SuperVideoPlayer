98a1801035a59dff480a0225a5a1bfeb107c439e
xiaoexiao51 <xiaoexiao51@qq.com>
2017-11-27 11:38:55 +0800

    添加截图和文档

--- app/src/main/java/com/rwzx/supervideoplayer/activity/NiceVideoActivity.java
+++ app/src/main/java/com/rwzx/supervideoplayer/activity/NiceVideoActivity.java
@@ -47,7 +47,7 @@ public class NiceVideoActivity extends AppCompatActivity {
         mNiceVideoPlayer.setUp("http://play.g3proxy.lecloud.com/vod/v2/MjUxLzE2LzgvbGV0di11dHMvMTQvdmVyXzAwXzIyLTExMDc2NDEzODctYXZjLTE5OTgxOS1hYWMtNDgwMDAtNTI2MTEwLTE3MDg3NjEzLWY1OGY2YzM1NjkwZTA2ZGFmYjg2MTVlYzc5MjEyZjU4LTE0OTg1NTc2ODY4MjMubXA0?b=259&mmsid=65565355&tm=1499247143&key=f0eadb4f30c404d49ff8ebad673d3742&platid=3&splatid=345&playid=0&tss=no&vtype=21&cvid=2026135183914&payff=0&pip=08cc52f8b09acd3eff8bf31688ddeced&format=0&sign=mb&dname=mobile&expect=1&tag=mobile&xformat=super", null);
         TxVideoPlayerController controller = new TxVideoPlayerController(this);
         controller.setTitle("态度决定一切，细节决定成败，态度决定一切，细节决定成败！");
-//        controller.isLive(true);
+        controller.isLive(false);
 //        controller.setClarity(getClarites(), 0);
 //        Glide.with(this).load("http://imgsrc.baidu.com/123.jpg").into(controller.imageView());
         mNiceVideoPlayer.setController(controller);
